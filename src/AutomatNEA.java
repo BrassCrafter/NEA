3b91323d4879b50375de1a3b75ee4a0ac4c6e1dd
BrassCrafter <tillmann-rohlfing@protonmail.com>
2024-02-28 20:33:45 +0100

    NEA optimiert und funktionstüchtig, nach grob einer Stunde Bug-Suche (Manch achte auf Folgendes: Zahlen die in einem switch-case mit char verwendet werden, sollte man mit '' versehen, sonst passiert da nichts)
    Took 57 minutes

--- src/AutomatNEA.java
+++ src/AutomatNEA.java
@@ -1,18 +1,24 @@
+import java.util.Objects;
+
 public class AutomatNEA {
 
     private Zustand startzustand;
     private List<Zustand> aktiveZustaende;
+    private String word;
 
     public class Q0 extends Zustand {
-        List<Zustand> folgezustaende1;
+
 
         public Q0() {
-            Q1 q1 = new Q1();
-            folgezustaende1 = new List<Zustand>();
-            folgezustaende1.append(q1);
+            super();
         }
 
         public List<Zustand> gibFolgezustaende1() {
+            List<Zustand> folgezustaende1;
+            Q1 q1 = new Q1();
+            folgezustaende1 = new List<>();
+            folgezustaende1.append(q1);
+            folgezustaende1.toFirst();
             return folgezustaende1;
         }
 
@@ -23,17 +29,19 @@ public class AutomatNEA {
     }
 
     public class Q1 extends Zustand {
-        List<Zustand> folgezustaende0;
+
 
         public Q1() {
-            Q0 q0 = new Q0();
-            Q2 q2 = new Q2();
-            folgezustaende0 = new List<Zustand>();
-            folgezustaende0.append(q0);
-            folgezustaende0.append(q2);
+            super();
         }
 
         public List<Zustand> gibFolgezustaende0() {
+            List<Zustand> folgezustaende0;
+            Q0 q0 = new Q0();
+            Q2 q2 = new Q2();
+            folgezustaende0 = new List<>();
+            folgezustaende0.append(q0);
+            folgezustaende0.append(q2);
             return folgezustaende0;
         }
 
@@ -44,17 +52,18 @@ public class AutomatNEA {
     }
 
     public class Q2 extends Zustand {
-        List<Zustand> folgezustaende1;
 
         public Q2() {
-            Q0 q0 = new Q0();
-            Q3 q3 = new Q3();
-            folgezustaende1 = new List<Zustand>();
-            folgezustaende1.append(q0);
-            folgezustaende1.append(q3);
+            super();
         }
 
         public List<Zustand> gibFolgezustaende1() {
+            List<Zustand> folgezustaende1;
+            Q0 q0 = new Q0();
+            Q3 q3 = new Q3();
+            folgezustaende1 = new List<>();
+            folgezustaende1.append(q0);
+            folgezustaende1.append(q3);
             return folgezustaende1;
         }
 
@@ -65,15 +74,15 @@ public class AutomatNEA {
     }
 
     public class Q3 extends Zustand {
-        List<Zustand> folgezustaende1;
 
         public Q3() {
-            Q0 q0 = new Q0();
-            folgezustaende1 = new List<Zustand>();
-            folgezustaende1.append(q0);
+            super();
         }
 
         public List<Zustand> gibFolgezustaende1() {
+            List<Zustand> folgezustaende1 = new List<>();
+            Q0 q0 = new Q0();
+            folgezustaende1.append(q0);
             return folgezustaende1;
         }
 
@@ -83,12 +92,55 @@ public class AutomatNEA {
 
     }
 
-    public void pruefeWort(String pWort) {
+    public AutomatNEA() {
+        word = "";
+        Q0 q0 = new Q0();
+        startzustand = q0;
+        aktiveZustaende = new List<>();
+        aktiveZustaende.append(startzustand);
+        aktiveZustaende.toFirst();
+    }
 
+    public void pruefeWort(String pWort) {
+        word = pWort;
+        while (!word.isEmpty()) {
+            char character = getCurrentChar();
+            List<Zustand> tempStateStorage = new List<>();
+            switch (character) {
+                case '0':
+                    for (aktiveZustaende.toFirst(); aktiveZustaende.hasAccess(); aktiveZustaende.next())
+                        tempStateStorage.concat(aktiveZustaende.getContent().gibFolgezustaende0());
+                    break;
+                case '1':
+                    for (aktiveZustaende.toFirst(); aktiveZustaende.hasAccess(); aktiveZustaende.next())
+                        tempStateStorage.concat(aktiveZustaende.getContent().gibFolgezustaende1());
+                    break;
+            }
+            aktiveZustaende = tempStateStorage;
+            aktiveZustaende.toFirst();
+        }
     }
 
     public boolean akzeptiert() {
+        for (aktiveZustaende.toFirst(); aktiveZustaende.hasAccess(); aktiveZustaende.next()) {
+            if (aktiveZustaende.getContent().istEndzustand()) return true;
+        }
         return false;
     }
 
+    private char getCurrentChar() {
+        char first = word.charAt(0);
+        word = word.substring(1);
+        return first;
+    }
+
+    public static void main(String[] args) {
+        AutomatNEA nea = new AutomatNEA();
+        String word = "";
+        nea.pruefeWort(word);
+        if (nea.akzeptiert()) System.out.println("Das Wort \"" + word + "\" wird von diesem Automaten akzeptiert.");
+        else {
+            System.out.println("Das Wort \"" + word + "\" wird von diesem Automaten nicht akzeptiert.");
+        }
+    }
 }
